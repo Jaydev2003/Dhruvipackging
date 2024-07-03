18f3538026fe38830c9fd1ea0fe323b4bc6a1666
Jaydev <116018824+Jaydev2003@users.noreply.github.com>
2024-07-03 10:19:26 +0530

    change

---
+++ js/main.js
@@ -0,0 +1,112 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner(0);
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.navbar').addClass('sticky-top shadow-sm');
+        } else {
+            $('.navbar').removeClass('sticky-top shadow-sm');
+        }
+    });
+
+
+    // testimonial carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        center: false,
+        dots: true,
+        loop: true,
+        margin: 25,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-right"></i>',
+            '<i class="fa fa-angle-left"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:1
+            },
+            992:{
+                items:2
+            },
+            1200:{
+                items:2
+            }
+        }
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 5,
+        time: 2000
+    });
+
+
+   // Back to top button
+   $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    //Add class in body tag when click on main menu btn
+    $(".navbar-toggler").click(function(){
+      $("body").toggleClass("overflow-hidden");
+    }); 
+
+})(jQuery);
+
+
+/*Side Navbar*/
+
+document.addEventListener('DOMContentLoaded', function () {
+    var navbarCollapse = document.getElementById('navbarCollapse');
+    var navbarIcon = document.getElementById('navbar-icon');
+    var navbarMainMenuButton = document.getElementsByClassName('navbar-mainmenu-button')[0];
+    var navtogglerbutton = document.getElementsByClassName('navbar-toggler')[0];
+    navbarCollapse.addEventListener('show.bs.collapse', function () {
+        navbarIcon.classList.remove('fa', 'fa-bars');
+        navbarIcon.classList.add('fas', 'fa-times');
+        navbarMainMenuButton.style.background = "#3e4095";
+        navtogglerbutton.style.color = '#fff'
+    });
+
+    navbarCollapse.addEventListener('hide.bs.collapse', function () {
+        navbarIcon.classList.remove('fas', 'fa-times');
+        navbarIcon.classList.add('fa', 'fa-bars');
+        navbarMainMenuButton.style.background = "none";
+        navtogglerbutton.style.color = '#000'
+    });
+});
